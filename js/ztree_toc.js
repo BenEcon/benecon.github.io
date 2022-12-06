3545da01147029502b4eddcb9c8eeb5fa1b98865
BenEcon <53943014+BenEcon@users.noreply.github.com>
2022-12-06 15:40:59 +0800

    Update 7 files from js/
    Update js/analytics.js
    Update js/bootstrap-toc.min.js
    Update js/bootstrap.min.js
    Update js/highlight.min.js
    Update js/jquery-1.4.4.min.js
    Update js/jquery-2.1.4.min.js
    Update js/ztree_toc.js

--- js/ztree_toc.js
+++ js/ztree_toc.js
@@ -4,10 +4,10 @@
  *
  * 1 = 0*100 +1
  */
-function encode_id_with_array(opts,arr) {
+function encode_id_with_array(opts, arr) {
 	var result = 0;
-	for(var z = 0; z < arr.length; z++ ) {
-		result += factor(opts, arr.length - z ,arr[z]);
+	for (var z = 0; z < arr.length; z++) {
+		result += factor(opts, arr.length - z, arr[z]);
 	}
 
 	return result;
@@ -23,47 +23,47 @@ function encode_id_with_array(opts,arr) {
  * 1,1 = 100
  *
  */
-function get_parent_id_with_array(opts,arr) {
+function get_parent_id_with_array(opts, arr) {
 	var result_arr = [];
 
-	for(var z = 0; z < arr.length; z++ ) {
+	for (var z = 0; z < arr.length; z++) {
 		result_arr.push(arr[z]);
 	}
 
 	result_arr.pop();
 
 	var result = 0;
-	for(var z = 0; z < result_arr.length; z++ ) {
-		result += factor(opts,result_arr.length - z,result_arr[z]);
+	for (var z = 0; z < result_arr.length; z++) {
+		result += factor(opts, result_arr.length - z, result_arr[z]);
 	}
 
 	return result;
 }
 
-function factor(opts ,count,current) {
-	if(1 == count) {
+function factor(opts, count, current) {
+	if (1 == count) {
 		return current;
 	}
 
 	var str = '';
-	for(var i = count - 1;i > 0; i-- ) {
-		str += current * opts.step+'*';
+	for (var i = count - 1; i > 0; i--) {
+		str += current * opts.step + '*';
 	}
 
-	return eval( str + '1' );
+	return eval(str + '1');
 }
 
-;(function($) {
+; (function ($) {
 	/*
 	 * 根据header创建目录内容
 	 */
 	function create_toc(opts) {
-		$(opts.documment_selector).find(':header').each(function() {
+		$(opts.documment_selector).find(':header').each(function () {
 			var level = parseInt(this.nodeName.substring(1), 10);
 
-			_rename_header_content(opts,this,level);
+			_rename_header_content(opts, this, level);
 
-			_add_header_node(opts,$(this));
+			_add_header_node(opts, $(this));
 		});//end each
 	}
 
@@ -72,19 +72,19 @@ function factor(opts ,count,current) {
 	 */
 	function render_with_ztree(opts) {
 		var t = $(opts._zTree);
-		t = $.fn.zTree.init(t,opts.ztreeSetting,opts._header_nodes).expandAll(opts.is_expand_all);
+		t = $.fn.zTree.init(t, opts.ztreeSetting, opts._header_nodes).expandAll(opts.is_expand_all);
 		// alert(opts._headers * 88);
 		// $(opts._zTree).height(opts._headers  * 33 + 33);
 
-		if(opts.is_posion_top == true){
+		if (opts.is_posion_top == true) {
 			opts.ztreeStyle.top = '0px';
 
-			if( opts.ztreeStyle.hasOwnProperty('bottom') )
-				delete opts.ztreeStyle.bottom ;
-		}else{
+			if (opts.ztreeStyle.hasOwnProperty('bottom'))
+				delete opts.ztreeStyle.bottom;
+		} else {
 			opts.ztreeStyle.bottom = '0px';
 
-			if( opts.ztreeStyle.hasOwnProperty('top') )
+			if (opts.ztreeStyle.hasOwnProperty('top'))
 				delete opts.ztreeStyle.top;
 		}
 
@@ -94,24 +94,24 @@ function factor(opts ,count,current) {
 	/*
 	 * 将已有header编号，并重命名
 	 */
-	function _rename_header_content(opts ,header_obj ,level) {
-		if(opts._headers.length == level) {
+	function _rename_header_content(opts, header_obj, level) {
+		if (opts._headers.length == level) {
 			opts._headers[level - 1]++;
-		} else if(opts._headers.length > level) {
+		} else if (opts._headers.length > level) {
 			opts._headers = opts._headers.slice(0, level);
-			opts._headers[level - 1] ++;
-		} else if(opts._headers.length < level) {
-			for(var i = 0; i < (level - opts._headers.length); i++) {
-			  // console.log('push 1');
-			  opts._headers.push(1);
+			opts._headers[level - 1]++;
+		} else if (opts._headers.length < level) {
+			for (var i = 0; i < (level - opts._headers.length); i++) {
+				// console.log('push 1');
+				opts._headers.push(1);
 			}
 		}
 
-		if(opts.is_auto_number == true) {
+		if (opts.is_auto_number == true) {
 			//另存为的文件里会有编号，所以有编号的就不再重新替换
-			if($(header_obj).text().indexOf( opts._headers.join('.') ) != -1){
+			if ($(header_obj).text().indexOf(opts._headers.join('.')) != -1) {
 
-			}else{
+			} else {
 				$(header_obj).text(opts._headers.join('.') + '. ' + $(header_obj).text());
 			}
 		}
@@ -122,7 +122,7 @@ function factor(opts ,count,current) {
 	 * this method can get a headable anchor
 	 * add by https://github.com/chanble
 	 */
-	function _get_anchor_from_head(header_obj){
+	function _get_anchor_from_head(header_obj) {
 		var name = header_obj.html();
 		var aname = name.replace(/\./g, "-");
 		var anchor = aname.trim();
@@ -134,32 +134,32 @@ function factor(opts ,count,current) {
 	/*
 	 * 给ztree用的header_nodes增加数据
 	 */
-	function _add_header_node(opts ,header_obj) {
-		var id  = encode_id_with_array(opts,opts._headers);//for ztree
-		var pid = get_parent_id_with_array(opts,opts._headers);//for ztree
+	function _add_header_node(opts, header_obj) {
+		var id = encode_id_with_array(opts, opts._headers);//for ztree
+		var pid = get_parent_id_with_array(opts, opts._headers);//for ztree
 		var anchor = id;//use_head_anchor.html#第二部分
 
 		// 默认使用标题作为anchor
-		if(opts.use_head_anchor == true){
+		if (opts.use_head_anchor == true) {
 			anchor = _get_anchor_from_head(header_obj);
 		}
 
 		// 设置锚点id
-		$(header_obj).attr('id',anchor);
+		$(header_obj).attr('id', anchor);
 
 		log($(header_obj).text());
 
 		opts._header_offsets.push($(header_obj).offset().top - opts.highlight_offset);
 
-		log('h offset ='+( $(header_obj).offset().top - opts.highlight_offset ) );
+		log('h offset =' + ($(header_obj).offset().top - opts.highlight_offset));
 
 		opts._header_nodes.push({
-			id:id,
-			pId:pid,
-			name:$(header_obj).text()||'null',
-			open:true,
-			url:'#'+ anchor,
-			target:'_self'
+			id: id,
+			pId: pid,
+			name: $(header_obj).text() || 'null',
+			open: true,
+			url: '#' + anchor,
+			target: '_self'
 		});
 	}
 
@@ -168,24 +168,24 @@ function factor(opts ,count,current) {
 	 */
 	function bind_scroll_event_and_update_postion(opts) {
 		var timeout;
-	    var highlight_on_scroll = function(e) {
+		var highlight_on_scroll = function (e) {
 			if (timeout) {
 				clearTimeout(timeout);
 			}
 
-			timeout = setTimeout(function() {
-				var top = $(opts.scroll_selector).scrollTop(),highlighted;
+			timeout = setTimeout(function () {
+				var top = $(opts.scroll_selector).scrollTop(), highlighted;
 
-				if(opts.debug) console.log('top='+top);
+				if (opts.debug) console.log('top=' + top);
 
 				for (var i = 0, c = opts._header_offsets.length; i < c; i++) {
 					// fixed: top+5防止点击ztree的时候，出现向上抖动的情况
-					if (opts._header_offsets[i] >= (top + 5) ) {
-						console.log('opts._header_offsets['+ i +'] = '+opts._header_offsets[i]);
+					if (opts._header_offsets[i] >= (top + 5)) {
+						console.log('opts._header_offsets[' + i + '] = ' + opts._header_offsets[i]);
 						$('a').removeClass('curSelectedNode');
 
 						// 由于有root节点，所以i应该从1开始
-				  		var obj = $('#tree_' + (i+1) + '_a').addClass('curSelectedNode');
+						var obj = $('#tree_' + (i + 1) + '_a').addClass('curSelectedNode');
 						break;
 					}
 				}
@@ -210,16 +210,16 @@ function factor(opts ,count,current) {
 	 */
 	function log(str) {
 		return;
-		if($.fn.ztree_toc.defaults.debug == true) {
+		if ($.fn.ztree_toc.defaults.debug == true) {
 			console.log(str);
 		}
 	}
 
-	$.fn.ztree_toc = function(options) {
+	$.fn.ztree_toc = function (options) {
 		// 将defaults 和 options 参数合并到{}
-		var opts = $.extend({},$.fn.ztree_toc.defaults,options);
+		var opts = $.extend({}, $.fn.ztree_toc.defaults, options);
 
-		return this.each(function() {
+		return this.each(function () {
 			opts._zTree = $(this);
 
 			// 初始化
@@ -232,7 +232,7 @@ function factor(opts ,count,current) {
 			render_with_ztree(opts);
 
 			// 根据滚动确定当前位置，并更新ztree
-		    bind_scroll_event_and_update_postion(opts);
+			bind_scroll_event_and_update_postion(opts);
 		});
 		// each end
 	}
@@ -277,7 +277,7 @@ function factor(opts ,count,current) {
 		is_highlight_selected_line: true,
 		step: 100,
 		ztreeStyle: {
-			width:'16%',
+			width: '16%',
 			overflow: 'auto',
 			position: 'fixed',
 			'z-index': 2147483647,
@@ -296,27 +296,27 @@ function factor(opts ,count,current) {
 			data: {
 				simpleData: {
 					enable: true,
-					idKey : "id",
+					idKey: "id",
 					pIdKey: "pId",
 					// rootPId: "0"
 				}
 			},
 			callback: {
-				beforeClick: function(treeId, treeNode) {
+				beforeClick: function (treeId, treeNode) {
 					$('a').removeClass('curSelectedNode');
-					if(treeNode.id == 1){
+					if (treeNode.id == 1) {
 						// TODO: when click root node
 						console.log('click root table of content');
 					}
-					if($.fn.ztree_toc.defaults.is_highlight_selected_line == true) {
-						$('#' + treeNode.id).css('color' ,'red').fadeOut("slow" ,function() {
-						    // Animation complete.
-							$(this).show().css('color','black');
+					if ($.fn.ztree_toc.defaults.is_highlight_selected_line == true) {
+						$('#' + treeNode.id).css('color', 'red').fadeOut("slow", function () {
+							// Animation complete.
+							$(this).show().css('color', 'black');
 						});
 					}
 				},
-				onRightClick: function(event, treeId, treeNode) {
-					if(treeNode.id == 1){
+				onRightClick: function (event, treeId, treeNode) {
+					if (treeNode.id == 1) {
 						// TODO: when right_click root node:table content
 						console.log('right_click root table of content');
 					}
